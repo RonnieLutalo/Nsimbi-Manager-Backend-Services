db763ec747d98cf45a044665b85d925b7df401c5
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-03-07 14:42:27 +0300

    Add domain models

---
+++ Domain/ExpenseCategory.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    internal class Category : BaseDomainEntity
+    {
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Domain/ExpenseDetails.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    internal class Expense : BaseDomainEntity
+    {
+        public string Description { get; set; }
+        public double Amount { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
