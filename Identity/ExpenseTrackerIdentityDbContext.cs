1d092ef7e3285219d96eddf29d2728a0f410961e
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-03-11 12:53:24 +0300

    Create ExpenseTrackerIdentityDbContext.cs

---
+++ Identity/ExpenseTrackerIdentityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Identity.Configurations;
+
+namespace Identity
+{
+    public class ExpenseTrackerIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ExpenseTrackerIdentityDbContext(DbContextOptions<ExpenseTrackerIdentityDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRoleConfiguration());
+        }
+    }
+}
