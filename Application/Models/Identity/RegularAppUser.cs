fc82fa0e6c801f7f543af7dd6286f4c80447afca
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-03-11 12:48:56 +0300

    Add Identity logic for application layer

---
+++ Application/Models/Identity/RegularAppUser.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Application.Models.Identity
+{
+    public class RegularAppUser
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
