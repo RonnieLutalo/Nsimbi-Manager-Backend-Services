88f181b80d402480893e0990aaec114721d07ff6
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-03-20 21:32:08 +0300

    Update Domain layer

---
+++ ExpenseTracker.Domain/ExpenseCategory.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseTracker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Domain
+{
+    public class ExpenseCategory : BaseDomainEntity
+    {
+        public string CategoryName { get; set; }
+    }
+}
