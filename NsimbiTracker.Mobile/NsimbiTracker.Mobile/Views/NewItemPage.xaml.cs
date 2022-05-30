4fd6f6b0807a6988b35f62ee49cddb11252a38eb
Ronnie Lutalo <ronnielutaro@gmail.com>
2022-05-30 22:10:19 +0300

    Initialize Mobile application

---
+++ NsimbiTracker.Mobile/NsimbiTracker.Mobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NsimbiTracker.Mobile.Models;
+using NsimbiTracker.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NsimbiTracker.Mobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
